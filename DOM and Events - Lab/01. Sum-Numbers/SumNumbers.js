d1fc5db88c930444a4a198d4d8b9f682b7cca3c9
kumchovylcho <tamerchoyy@gmail.com>
2023-06-25 14:50:55 +0300

    initial solution

---
+++ DOM and Events - Lab/01. Sum-Numbers/SumNumbers.js
@@ -0,0 +1,6 @@
+function calc() {
+    const numOne = parseInt(document.getElementById('num1').value)
+    const numTwo = parseInt(document.getElementById('num2').value)
+
+    document.getElementById('sum').value = numOne + numTwo
+}
