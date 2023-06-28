41b443991b447b1ac888f195aea38712b78d513a
kumchovylcho <tamerchoyy@gmail.com>
2023-06-28 18:07:32 +0300

    initial solution

---
+++ DOM and Events - Exercises/07. Format the Text/solution.js
@@ -0,0 +1,32 @@
+function solve() {
+    const mainDiv = document.getElementById("output")
+
+    const getText = document.getElementById("input")
+
+    const sentences = getText.value.split(".")
+
+    let everyThree = []
+
+
+    for (let sentence of sentences) {
+        if (sentence.length > 0) {
+            everyThree.push(sentence)
+        }
+
+        if (everyThree.length === 3) {
+            const paragraph = document.createElement("p")
+            paragraph.textContent = everyThree.join(".").trim() + "."
+
+            mainDiv.appendChild(paragraph)
+            everyThree = []
+        }
+    }
+
+    if (everyThree.length > 0) {
+        const paragraph = document.createElement("p")
+        paragraph.textContent = everyThree.join(".").trim() + "."
+
+        mainDiv.appendChild(paragraph)
+    }
+
+}
