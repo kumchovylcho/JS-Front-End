7e530d6c0ba3fe0a6912c47475ebb508f1e2073c
Tamer <tamerchoyy@gmail.com>
2023-09-26 17:01:21 +0300

    fill

---
+++ JS Arrays and Strings-More Exercise/01_Login.js
@@ -0,0 +1,31 @@
+function login(loginData) {
+
+    function reverseString(string) {
+        let result = []
+        for (let i = string.length - 1; i >= 0; i--) {
+            result.push(string[i])
+        }
+
+        return result.join("")
+    }
+
+    let username = loginData[0]
+    let tries = 0
+
+    for (let i = 1; i < loginData.length; i++) {
+        tries++
+
+        if (username !== reverseString(loginData[i])) {
+            if (tries === 4) {
+                console.log(`User ${username} blocked!`)
+                break
+            }
+            console.log("Incorrect password. Try again.")
+        }
+
+        else {
+            console.log(`User ${username} logged in.`)
+            break
+        }
+    }
+}
