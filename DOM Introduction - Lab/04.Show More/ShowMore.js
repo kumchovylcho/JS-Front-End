954872fa53e2fc171cf48b0abab56a901261063f
Tamer <tamerchoyy@gmail.com>
2023-09-26 17:53:12 +0300

    initial solution

---
+++ DOM Introduction - Lab/04.Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    const link = document.querySelector("#more");
+    const spanText = document.querySelector("#text");
+
+    link.style.display = "none";
+    spanText.style.display = "inline";
+}
