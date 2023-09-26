31d07188821b0c835a85d96b8bb14a3780609f7f
Tamer <tamerchoyy@gmail.com>
2023-09-26 17:53:36 +0300

    initial solution

---
+++ DOM Introduction - Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -0,0 +1,8 @@
+function extract(content) {
+    const text = document.getElementById(content).textContent
+
+    const pattern = /\([\w ]+\)/g
+    const result = text.match(pattern)
+
+    return result.join("; ")
+}
