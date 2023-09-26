5956a6e03f0d80dd6d3f18bccae61cd2a9305942
Tamer <tamerchoyy@gmail.com>
2023-09-26 17:53:20 +0300

    initial solution

---
+++ DOM Introduction - Lab/05.Colorize Table/ColorizeTable.js
@@ -0,0 +1,11 @@
+function colorize() {
+    const table = document.getElementsByTagName("table")[0]
+
+    for (let i = 0; i < table.rows.length; i++) {
+        if (i % 2 === 0) {
+            continue
+        }
+
+        table.rows[i].style.backgroundColor = "Teal"
+    }
+}
