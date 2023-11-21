3188badd3dde0187505cc65550e0439fdfd3a200
Tamer <tamerchoyy@gmail.com>
2023-11-22 00:18:51 +0200

    ref

--- Objects and Classes - Lab/10_songs.js
+++ Objects and Classes - Lab/10_songs.js
@@ -17,12 +17,42 @@ function solve(info) {
         songs.push(new Song(type, name, time))
     }
 
-    if (typeSong === "all") {
-        songs.forEach((x) => console.log(x.name))
-    }
+    const outputSongs = typeSong === "all"
+        ? songs
+        : songs.filter(song => song.typeList === typeSong)
 
-    else {
-        let filterSongs = songs.filter((x) => x.typeList === typeSong)
-        filterSongs.forEach((x) => console.log(x.name))
-    }
+    outputSongs.forEach(song => console.log(song.name))
 }
+
+
+
+
+
+// function solve(info) {
+//
+//     class Song {
+//         constructor(typeList, name, time) {
+//             this.typeList = typeList
+//             this.name = name
+//             this.time = time
+//         }
+//     }
+//
+//     let countSongs = info.shift()
+//     let typeSong = info.pop()
+//     let songs = []
+//
+//     for (let i = 0; i < countSongs; i++) {
+//         let [type, name, time] = info[i].split("_")
+//         songs.push(new Song(type, name, time))
+//     }
+//
+//     if (typeSong === "all") {
+//         songs.forEach((x) => console.log(x.name))
+//     }
+//
+//     else {
+//         let filterSongs = songs.filter((x) => x.typeList === typeSong)
+//         filterSongs.forEach((x) => console.log(x.name))
+//     }
+// }
